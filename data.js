3815a88a619a20c287f0de2d6b6ef8e40a75e98e
Mukil081 <147730492+Mukil081@users.noreply.github.com>
2024-08-14 08:29:06 +0530

    Files

---
+++ data.js
@@ -0,0 +1,51 @@
+let songs = [
+    {
+        name: 'Nee Kavithaigala',
+        path: 'music/Nee kavithaigala.mp3',
+        artist: 'Pradeep Kumar',
+        cover: 'images/cover1.jpg'
+    },
+    {
+        name: 'Illuminati',
+        path: 'music/Illuminati.mp3',
+        artist: 'Sushin Shyam',
+        cover: 'images/cover2.jpg'
+    },
+    {
+        name: 'Hey Nijame',
+        path: 'music/Hey Nijame.mp3',
+        artist: 'Darbuka Siva',
+        cover: 'images/cover3.jpg'
+    },
+    {
+        name: 'Thaensudare',
+        path: 'music/Thaensudare.mp3',
+        artist: 'Sean Roldan',
+        cover: 'images/cover4.jpg'
+    },
+    {
+        name: 'Raanjhanaa',
+        path: 'music/Raanjhanaa.mp3',
+        artist: 'A.R. Rahman',
+        cover: 'images/cover5.jpg'
+    },
+    {
+        name: 'Perfect',
+        path: 'music/Perfect.mp3',
+        artist: 'Ed Sheeran',
+        cover: 'images/cover6.jpg'
+    },
+    {
+        name: 'Aasa Kooda',
+        path: 'music/Aasa Kooda.mp3',
+        artist: 'Sai Abhyankkar',
+        cover: 'images/cover7.jpg'
+    },
+    {
+        name: 'Satranga',
+        path: 'music/Satranga.mp3',
+        artist: 'Arijit Singh',
+        cover: 'images/cover9.jpg'
+    },
+    
+]
